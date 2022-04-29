9b0c8598bfba16694a2fd30c46c8cd04f0874ac4
Tomás Elizondo <tomaslelizondo@gmail.com>
2022-04-29 01:36:07 -0300

    [BREAKING CHANGE] custom react hook for persistency

--- src/App/App.js
+++ src/App/App.js
@@ -12,20 +12,31 @@ import { TodoButton } from "../components/TodoButton";
 //   { text: 'Tomar el curso de intro a react', completed: true },
 //   { text: 'Llorar con la llorona', completed: false },
 // ];
+
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItems;
+  
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItems = initialValue;
+  } else {
+    parsedItems = JSON.parse(localStorageItem);
+  }
+  
+  const [item, setItem] = React.useState(parsedItems)
+
+  const saveItem = (newItem) => {
+    const stringifiedItem = JSON.stringify(newItem);
+    localStorage.setItem(itemName, stringifiedItem);
+    setItem(newItem);
+  };
+
+  return [item, saveItem];
+}
  
 function App() {
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
-  let parsedTodos;
-  
-  if (!localStorageTodos) {
-    localStorage.setItem('TODOS_V1', JSON.stringify([]));
-    parsedTodos = [];
-  } else {
-    parsedTodos = JSON.parse(localStorageTodos);
-  }
-
-
-  const [todos, setTodos] = React.useState(parsedTodos)
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -43,11 +54,7 @@ function App() {
     });
   }
 
-  const saveTodos = (newTodos) => {
-    const stringifiedTodos = JSON.stringify(newTodos);
-    localStorage.setItem('TODOS_V1', stringifiedTodos);
-    setTodos(newTodos);
-  };
+  
 
   const completeTodos = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
